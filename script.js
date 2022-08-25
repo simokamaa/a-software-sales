9e1de8ccbbf285bd36e1b6309fd770c035897796
Simon Kamau <simonkamau690@gmail.com>
2022-08-25 15:40:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function nav() {
+	document.getElementById("nav").classList.toggle("nav-full");
+	document.getElementById("hamburger").classList.toggle("turned");
+	document.getElementById("one").classList.toggle("turned-one");
+	document.getElementById("two").classList.toggle("turned-two");
+	console.log("hi")
+}
